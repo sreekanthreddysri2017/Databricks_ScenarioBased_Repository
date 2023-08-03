040bcc6b4c572aeef91596df90b5ee25e83bc278
sreekanthreddysri2017@gmail.com <sreekanthreddysri2017@gmail.com>
2023-08-03 05:49:54 +0000

    commit_1

---
+++ Adding_columns_dynamically.py
@@ -0,0 +1,31 @@
+# Databricks notebook source
+#Add a column to a dataFrame dynamically.
+
+# COMMAND ----------
+
+# Databricks notebook source
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType
+from pyspark.sql.functions import *
+
+data2 = [("James","","Smith","36636","M",3000),
+    ("Michael","Rose","","40288","M",4000),
+    ("Robert","","Williams","42114","M",4000),
+    ("Maria","Anne","Jones","39192","F",4000),
+    ("Jen","Mary","Brown","","F",1000)
+  ]
+
+schema = StructType([ \
+    StructField("firstname",StringType(),True), \
+    StructField("middlename",StringType(),True), \
+    StructField("lastname",StringType(),True), \
+    StructField("id", StringType(), True), \
+    StructField("gender", StringType(), True), \
+    StructField("salary", IntegerType(), True) \
+  ])
+ 
+df = spark.createDataFrame(data=data2,schema=schema)
+display(df)
+
+# COMMAND ----------
+
+
